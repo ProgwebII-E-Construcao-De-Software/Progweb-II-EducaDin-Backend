61044636219bd0f3b04f349a3b1f7dc03121f94b
Sara Gonçalves De Lima <saragdl7321@gmail.com>
2024-08-27 10:15:37 -0300

    UPDATE - Configurando maven, criando packages do core da aplicação e definindo senha genérica do spring security

---
+++ src/main/java/com/g2/Progweb_II_EducaDin_Backend/ProgwebIiEducaDinBackendApplication.java
@@ -0,0 +1,13 @@
+package com.g2.Progweb_II_EducaDin_Backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgwebIiEducaDinBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProgwebIiEducaDinBackendApplication.class, args);
+	}
+
+}
