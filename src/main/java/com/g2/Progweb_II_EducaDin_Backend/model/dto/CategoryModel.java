3b9b65e1ef1cd4686762201b25ca32a6b1bae106
Guilherme Bastos <guilhermedoads@gmail.com>
2024-09-20 21:52:05 -0300

    FIX [Modelo e Reposiório] - Criando o modelo e repositorio de Category

---
+++ src/main/java/com/g2/Progweb_II_EducaDin_Backend/model/dto/CategoryModel.java
@@ -0,0 +1,44 @@
+package com.g2.Progweb_II_EducaDin_Backend.model.dto;
+
+import br.ueg.progweb2.arquitetura.model.GenericModel;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class CategoryModel implements GenericModel<Long> {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private boolean expense;
+
+    @Override
+    public Long getId() {
+        return id;
+    }
+
+    @Override
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        return "Category{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", isExpense=" + expense +
+                '}';
+    }
+}
