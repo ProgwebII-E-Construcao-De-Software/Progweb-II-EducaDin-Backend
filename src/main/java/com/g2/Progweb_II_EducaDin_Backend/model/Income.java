5debecee86ee5134b560737a5499800a7f10b12c
Guilherme Bastos <guilhermedoads@gmail.com>
2024-09-29 17:36:25 -0300

    FIX [Conflito de versionamento] - branchGuilherme

--- src/main/java/com/g2/Progweb_II_EducaDin_Backend/model/Income.java
+++ src/main/java/com/g2/Progweb_II_EducaDin_Backend/model/Income.java
@@ -1,6 +1,6 @@
 package com.g2.Progweb_II_EducaDin_Backend.model;
 
-import br.ueg.progweb2.arquitetura.annotations.MandatoryField;
+import br.ueg.progweb2.arquitetura.model.MandatoryField;
 import br.ueg.progweb2.arquitetura.model.GenericModel;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
---
+++ src/main/java/com/g2/Progweb_II_EducaDin_Backend/repository/IncomeRepository.java
@@ -0,0 +1,11 @@
+package com.g2.Progweb_II_EducaDin_Backend.repository;
+
+import com.g2.Progweb_II_EducaDin_Backend.model.Income;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface IncomeRepository extends JpaRepository<Income, Long> {
+}
