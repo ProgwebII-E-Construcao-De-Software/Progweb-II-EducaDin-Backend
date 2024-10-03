3ff6829bb6af87c750b5b5caf4e2e266fdaf99b4
Rafael Guibson <rafaelguibsons@gmail.com>
2024-10-03 00:04:06 -0300

    [Adicionado Controller]: Implementado controller e ajustado service.

--- src/main/java/com/g2/Progweb_II_EducaDin_Backend/service/ExpenseService.java
+++ src/main/java/com/g2/Progweb_II_EducaDin_Backend/service/ExpenseService.java
@@ -1,4 +1,7 @@
 package com.g2.Progweb_II_EducaDin_Backend.service;
 
-public interface ExpenseService {
+import br.ueg.progweb2.arquitetura.service.CrudService;
+import com.g2.Progweb_II_EducaDin_Backend.model.Expense;
+
+public interface ExpenseService extends CrudService<Expense, Long> {
 }
