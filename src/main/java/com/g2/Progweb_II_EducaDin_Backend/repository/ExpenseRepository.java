a564aef2233782fad3452642534685b926c51c21
Rafael Guibson <rafaelguibsons@gmail.com>
2024-10-03 19:34:14 -0300

    [Ajuste dos metodos Services]: ajustado service.

--- src/main/java/com/g2/Progweb_II_EducaDin_Backend/repository/ExpenseRepository.java
+++ src/main/java/com/g2/Progweb_II_EducaDin_Backend/repository/ExpenseRepository.java
@@ -4,7 +4,9 @@ import com.g2.Progweb_II_EducaDin_Backend.model.Expense;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ExpenseRepository extends JpaRepository<Expense, Long> {
-
+    List<Expense> findAllByName(String name);
 }
