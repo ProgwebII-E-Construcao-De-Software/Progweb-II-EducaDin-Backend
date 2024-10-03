ffb6a418ee85d6b73401cf041c2b9f83bb36cec2
Rafael Guibson <rafaelguibsons@gmail.com>
2024-10-02 23:57:11 -0300

    [Adicionado Mapper]: Implementado mapper do expense.

---
+++ src/main/java/com/g2/Progweb_II_EducaDin_Backend/mapper/ExpenseMapper.java
@@ -0,0 +1,33 @@
+package com.g2.Progweb_II_EducaDin_Backend.mapper;
+
+import br.ueg.progweb2.arquitetura.mapper.GenericMapper;
+import com.g2.Progweb_II_EducaDin_Backend.model.Expense;
+import com.g2.Progweb_II_EducaDin_Backend.model.dto.*;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Named;
+
+@Mapper(componentModel = "spring", uses = CategoryMapper.class)
+public interface ExpenseMapper extends GenericMapper<ExpenseDTO, ExpenseDTOCreateUpdate, ExpenseDTOCreateUpdate, ExpenseListDTO, Expense, Long> {
+
+    @Mapping(source = "categoryName", target = "category.name")
+    Expense fromDTOtoModel(ExpenseDTOCreateUpdate dto);
+
+    @Override
+    @Mapping(source = "categoryName", target = "category.name")
+    Expense fromDTOCreateToModel(ExpenseDTOCreateUpdate dto);
+
+    @Override
+    @Mapping(source = "category.name", target = "categoryName")
+    ExpenseDTO fromModeltoDTO(Expense model);
+
+    @Override
+    @Named(value = "toDTOList")
+    ExpenseListDTO toDTOList(Expense model);
+
+    @Override
+    @Mapping(source = "categoryName", target = "category.name")
+    Expense fromDTOUpdateToModel(ExpenseDTOCreateUpdate ExpenseDTOCreateUpdate);
+
+
+}
