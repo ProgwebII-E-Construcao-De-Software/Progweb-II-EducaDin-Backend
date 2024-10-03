018eead58a78cadeec6bc7fd6ddd682f70794881
Rafael Guibson <rafaelguibsons@gmail.com>
2024-10-03 08:01:58 -0300

    [Ajuste Método ToString e correção de endpoint]

--- src/main/java/com/g2/Progweb_II_EducaDin_Backend/controller/ExpenseController.java
+++ src/main/java/com/g2/Progweb_II_EducaDin_Backend/controller/ExpenseController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping(path = "${api.version}/incomes")
+@RequestMapping(path = "${api.version}/expenses")
 @CrossOrigin()
 public class ExpenseController extends GenericCRUDController<
         ExpenseDTO,
