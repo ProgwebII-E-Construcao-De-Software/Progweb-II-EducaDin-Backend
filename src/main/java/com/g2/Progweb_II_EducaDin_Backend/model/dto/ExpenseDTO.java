1e6d854ff76da8eb37a65174cfe8a0a8647afc9d
Rafael Guibson <rafaelguibsons@gmail.com>
2024-10-02 23:46:48 -0300

    [Criação DTOs]: Adicionado os DTOs de list, create, update

--- src/main/java/com/g2/Progweb_II_EducaDin_Backend/model/dto/ExpenseDTO.java
+++ src/main/java/com/g2/Progweb_II_EducaDin_Backend/model/dto/ExpenseDTO.java
@@ -1,10 +1,13 @@
 package com.g2.Progweb_II_EducaDin_Backend.model.dto;
 
-import com.g2.Progweb_II_EducaDin_Backend.model.Category;
+
+import java.time.LocalDate;
 
 public record ExpenseDTO(Long id,
-                         Category category,
-                         String name,
-                         String description,
-                         Double amount) {
+                        String categoryName,
+                        String name,
+                        String description,
+                        Double amount,
+                        Integer leadTime,
+                        LocalDate incomeDate) {
 }
---
+++ src/main/java/com/g2/Progweb_II_EducaDin_Backend/model/dto/ExpenseListDTO.java
@@ -0,0 +1,13 @@
+package com.g2.Progweb_II_EducaDin_Backend.model.dto;
+
+import com.g2.Progweb_II_EducaDin_Backend.model.Category;
+
+import java.time.LocalDate;
+
+public record ExpenseListDTO(Long id,
+                             Category category,
+                             String name,
+                             String description,
+                             Double amount,
+                             Integer leadTime,
+                             LocalDate incomeDate) { }
