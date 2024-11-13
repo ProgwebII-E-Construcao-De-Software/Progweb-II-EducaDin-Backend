be8d6f682cf3e1cea0b9896a0870621f5185e218
Rafael Guibson <rafaelguibsons@gmail.com>
2024-11-12 21:47:31 -0300

    CDS-67 - Refatorando arquitetura

--- src/main/java/com/g2/Progweb_II_EducaDin_Backend/ProgwebIiEducaDinBackendApplication.java
+++ src/main/java/com/g2/Progweb_II_EducaDin_Backend/ProgwebIiEducaDinBackendApplication.java
@@ -2,8 +2,21 @@ package com.g2.Progweb_II_EducaDin_Backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.convert.threeten.Jsr310JpaConverters;
 
-@SpringBootApplication
+@SpringBootApplication(
+		scanBasePackages = {
+				"com.g2.Progweb_II_EducaDin_Backend*",
+				//Para funcionamento da Arquitetura
+				"br.ueg.progweb2.arquitetura.*"}
+)
+@EntityScan(basePackageClasses = { Jsr310JpaConverters.class },
+		basePackages = {
+				"com.g2.Progweb_II_EducaDin_Backend*",
+				//Para funcionamento da Arquitetura
+				"br.ueg.progweb2.arquitetura.*"}
+)
 public class ProgwebIiEducaDinBackendApplication {
 
 	public static void main(String[] args) {
