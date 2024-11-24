962bef8f8c9bbadd996962dba52023b879712022
Rafael Guibson <rafaelguibsons@gmail.com>
2024-11-24 08:38:30 -0300

    [COMMIT 06] - Configurada e Haibilitada a segurança para os endpoints de incomes e expenses.

--- src/main/java/com/g2/Progweb_II_EducaDin_Backend/auth/security/SecurityConfigurations.java
+++ src/main/java/com/g2/Progweb_II_EducaDin_Backend/auth/security/SecurityConfigurations.java
@@ -2,6 +2,7 @@ package com.g2.Progweb_II_EducaDin_Backend.auth.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -15,6 +16,11 @@ public class SecurityConfigurations {
         return httpSecurity
                 .csrf(httpSecurityCsrfConfigurer -> httpSecurityCsrfConfigurer.disable())
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers(HttpMethod.POST, "/1.0/incomes").hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.POST, "/1.0/expenses").hasRole("ADMIN")
+                        .anyRequest().authenticated()
+                )
                 .build();
     }
 }
