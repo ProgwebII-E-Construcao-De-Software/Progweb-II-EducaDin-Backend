3437336b431758d780d09f4cd095e80e0d021ccf
Rafael Guibson <rafaelguibsons@gmail.com>
2024-11-24 06:47:10 -0300

    [COMMIT 01] - IMPLEMENTAÇÃO DA ENTIDADE DE USUÁRIO

---
+++ src/main/java/com/g2/Progweb_II_EducaDin_Backend/model/Users.java
@@ -0,0 +1,31 @@
+package com.g2.Progweb_II_EducaDin_Backend.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Builder
+@Table(name = "users")
+public class Users {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String username;
+
+    @Column(unique=true, nullable = false)
+    private String email;
+
+    @Column(unique=true, nullable = false)
+    private String password;
+
+}
