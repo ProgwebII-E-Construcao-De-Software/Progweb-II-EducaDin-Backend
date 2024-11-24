24cecdd1ccbeda4eb17d7f02cb6b35e0b8788dc9
Rafael Guibson <rafaelguibsons@gmail.com>
2024-11-24 07:13:26 -0300

    [COMMIT 03] - Repository adicionado.

---
+++ src/main/java/com/g2/Progweb_II_EducaDin_Backend/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.g2.Progweb_II_EducaDin_Backend.repository;
+
+import com.g2.Progweb_II_EducaDin_Backend.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    UserDetails findByLogin(String login);
+}
