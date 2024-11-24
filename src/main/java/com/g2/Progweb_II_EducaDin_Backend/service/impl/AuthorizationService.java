96ad4965d9a0d85023e127e823d82757a76528ab
Rafael Guibson <rafaelguibsons@gmail.com>
2024-11-24 07:49:32 -0300

    [COMMIT 04] - Criado o AuthorizationService e implementado o metodo load.

---
+++ src/main/java/com/g2/Progweb_II_EducaDin_Backend/service/impl/AuthorizationService.java
@@ -0,0 +1,18 @@
+package com.g2.Progweb_II_EducaDin_Backend.service.impl;
+
+import com.g2.Progweb_II_EducaDin_Backend.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+public class AuthorizationService  implements UserDetailsService {
+
+    @Autowired
+    UserRepository repository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return repository.findByUsername(username);
+    }
+}
