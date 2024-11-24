e6870106441c42ea39e677a7faf4a6397ce8b931
Rafael Guibson <rafaelguibsons@gmail.com>
2024-11-24 07:06:50 -0300

    [COMMIT 02] - Implementação do metodo getAuthorities e adição das roles como Enum.

---
+++ src/main/java/com/g2/Progweb_II_EducaDin_Backend/enums/UserRole.java
@@ -0,0 +1,16 @@
+package com.g2.Progweb_II_EducaDin_Backend.enums;
+
+public enum UserRole {
+    ADMIN("admin"),
+    USER("user");
+
+    private String role;
+
+    UserRole(String role) {
+        this.role = role;
+    }
+
+    public String getRole() {
+        return role;
+    }
+}
