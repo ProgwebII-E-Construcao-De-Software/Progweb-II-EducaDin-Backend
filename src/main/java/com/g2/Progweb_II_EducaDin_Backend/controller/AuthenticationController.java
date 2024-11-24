215b6a89175eb1d7a501eccf2c99a4d5dc4a9533
Rafael Guibson <rafaelguibsons@gmail.com>
2024-11-24 09:14:09 -0300

    [COMMIT 07] - Criado AuthenticationDTO e AuthenticationController com o endpoint do metodo de login.

---
+++ src/main/java/com/g2/Progweb_II_EducaDin_Backend/controller/AuthenticationController.java
@@ -0,0 +1,23 @@
+package com.g2.Progweb_II_EducaDin_Backend.controller;
+
+import com.g2.Progweb_II_EducaDin_Backend.model.dto.AuthenticationDTO;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("auth")
+public class AuthenticationController {
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @PostMapping("/login")
+    public ResponseEntity login(@RequestBody @Valid AuthenticationDTO data) {
+        var usernamePassword = new UsernamePasswordAuthenticationToken(data.username(), data.password());
+        var auth = authenticationManager.authenticate(usernamePassword);
+        return ResponseEntity.ok().build();
+    }
+}
