5042d7b5a83fa84d6467867b706b53177b541679
Rafael Guibson <rafaelguibsons@gmail.com>
2024-12-09 16:52:14 -0300

    [commit 05] - Criada Query para listar ambas as metas do owner a as que são compartilhadas com ele.

--- src/main/java/com/g2/Progweb_II_EducaDin_Backend/repository/GoalRepository.java
+++ src/main/java/com/g2/Progweb_II_EducaDin_Backend/repository/GoalRepository.java
@@ -6,6 +6,7 @@ import com.g2.Progweb_II_EducaDin_Backend.model.Income;
 import com.g2.Progweb_II_EducaDin_Backend.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -14,4 +15,15 @@ import java.util.List;
 public interface GoalRepository extends JpaRepository<Goal, Long> , JpaSpecificationExecutor<Goal> {
     Goal findByNameEqualsIgnoreCase(String name);
     List<Goal> findAllByUserId(long userId);
+
+    /**
+     * Busca metas do proprietário e metas compartilhadas com ele.
+     *
+     * @param userId ID do usuário.
+     * @return Lista de metas.
+     */
+    @Query("SELECT g FROM Goal g " +
+            "WHERE g.owner.id = :userId " +
+            "   OR :userId IN (SELECT sharedUser.id FROM g.sharedUsers sharedUser)")
+    List<Goal> findGoalsByOwnerOrSharedWith(Long userId);
 }
